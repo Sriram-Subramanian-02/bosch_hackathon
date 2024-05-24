6ed906b2108c3cf80743c289b053dec4bbcb4434
Sriram-Subramanian-02 <sriramsubramanian1968@gmail.com>
2024-05-24 15:57:48 +0530

    Add support for image extraction from vector db and mongodb atlas

--- constants.py
+++ constants.py
@@ -3,7 +3,8 @@ import numpy as np
 
 USER_ID = np.random.randint(10, 100000)
 SESSION_ID = np.random.randint(10, 100000)
-COHERE_API_KEY = "g6WqGnL6XZVDQURCNwy2xtCTqEiihXr7nIZhL2UV"
-QDRANT_URL = "https://8803fa99-7551-4f88-84c3-e134c9bed5de.us-east4-0.gcp.cloud.qdrant.io:6333"
-QDRANT_API_KEY = "EFeN_UhdmAlDNYZHqJBUbZ88Nt7N0MkmvWLgM5Hs4ogNvExLMwNwdQ"
+COHERE_API_KEY = "V5jqL0gNm1xo9gIzN0x32c9Nc814b4gh9gpOY6EY"
+QDRANT_URL = "https://35ebdc7d-ec99-4ebd-896c-ff5705cf369b.us-east4-0.gcp.cloud.qdrant.io:6333"
+QDRANT_API_KEY = "9dKJsKOYwT0vGlWPrZXBSIlbUzvRdJ1XkM0_floo8FmYCOHX_Y0y-Q"
+QDRANT_COLLECTION_NAME = "owners_manual_test"
 MONGO_DB_URL = "mongodb+srv://sriram:Ayynar%40123@msd.ywfrjgy.mongodb.net/?retryWrites=true&w=majority"
