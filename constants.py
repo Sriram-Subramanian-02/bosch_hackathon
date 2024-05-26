7a90f642e148f49cd1ff8b698bdab7952ecd4b5e
Sriram-Subramanian-02 <sriramsubramanian1968@gmail.com>
2024-05-26 19:01:42 +0530

    Bug fixes

--- constants.py
+++ constants.py
@@ -3,8 +3,10 @@ import numpy as np
 
 USER_ID = np.random.randint(10, 100000)
 SESSION_ID = np.random.randint(10, 100000)
-COHERE_API_KEY = "8Coue2HcZqaJ1pAvM8NJ6XbriT76IfhsPEnq2OGE"
-QDRANT_URL = "https://35ebdc7d-ec99-4ebd-896c-ff5705cf369b.us-east4-0.gcp.cloud.qdrant.io:6333"
-QDRANT_API_KEY = "9dKJsKOYwT0vGlWPrZXBSIlbUzvRdJ1XkM0_floo8FmYCOHX_Y0y-Q"
-QDRANT_COLLECTION_NAME = "owners_manual_test"
+COHERE_API_KEY_1 = "8y2cASrkSAGav1wi6VTlubFBMp7V1XRhMsy72leR"
+COHERE_API_KEY_2 = "xsVMq3dnuekhW5miz2Cq0HLXAMGnAjXwuSM9PDtk"
+COHERE_API_KEY = "P6t9MllaPbgvUFgMVhA3VGnrSYfAE3AP1PDdZdV9"
+QDRANT_URL = "https://fb64a3b5-b779-4f67-a0f8-d7ba73115573.us-east4-0.gcp.cloud.qdrant.io:6333"
+QDRANT_API_KEY = "iH7pzboYIG7T0GR4YUGGV6Zldj5iwADTPuKVNSt3UCs-ytDiKqfVzg"
+QDRANT_COLLECTION_NAME = "manuals-temp"
 MONGO_DB_URL = "mongodb+srv://sriram:Ayynar%40123@msd.ywfrjgy.mongodb.net/?retryWrites=true&w=majority"
