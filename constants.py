a73cd2abbba676ef050cc429fdea43c6c0ad9f4c
Sriram-Subramanian-02 <sriramsubramanian1968@gmail.com>
2024-06-02 21:01:28 +0530

    Change api keys for testing in prod

--- constants.py
+++ constants.py
@@ -5,7 +5,7 @@ USER_ID = np.random.randint(10, 100000)
 SESSION_ID = np.random.randint(10, 100000)
 COHERE_API_KEY_TEXT = "xsVMq3dnuekhW5miz2Cq0HLXAMGnAjXwuSM9PDt"
 COHERE_API_KEY_IMAGES = "xsVMq3dnuekhW5miz2Cq0HLXAMGnAjXwuSM9PDtk"
-COHERE_API_KEY_TABLES = "P6t9MllaPbgvUFgMVhA3VGnrSYfAE3AP1PDdZdV9"
+COHERE_API_KEY_TABLES = "xsVMq3dnuekhW5miz2Cq0HLXAMGnAjXwuSM9PDtk"
 
 pdf_mapping = {
     "Hyundai Exter": "hyundai_exter.pdf",
