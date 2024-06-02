fcbd959751d9df2eadab9ecaf4fa974bbfa98303
Sriram-Subramanian-02 <sriramsubramanian1968@gmail.com>
2024-06-02 11:52:54 +0530

    Update prompt to fix car names issue

--- services.py
+++ services.py
@@ -98,7 +98,7 @@ def get_response(query, threshold=0.35):
                 As similarity between query and context is low, try to ask several probing questions.
                 Ask several followup questions to get further clarity.
                 Answer in a polite tone, and convey to the user that you need more clarity to answer the question.
-                If the user doesnot specify the car's name, kindly ask for it as a probing question(available car names are HYUNDAI EXTER and TATA NEXON).
+                If the user doesnot specify the car's name, kindly ask for it as a probing question(available car names are HYUNDAI EXTER or EXTER, HYUNDAI VERNA or VERNA, TATA PUNCH or PUNCH and TATA NEXON).
                 Then display the probing questions as bulletin points.
                 Do not use technical words, give easy to understand responses.
                 If the question asked is a generic question or causal question answer them without using the context.
